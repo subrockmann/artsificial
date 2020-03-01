380f767335817e3135ba2ca3040d65d4cf6ccf25
Kapil Chandorikar <kapilchandorikar@gmail.com>
2020-03-02 01:00:57 +0530

    docs: Create the descriptions for the commands

--- ppn_app.py
+++ ppn_app.py
@@ -7,6 +7,16 @@ def get_args():
     Gets the arguments from the command line.
     '''
     parser = argparse.ArgumentParser("Run inference")
+    # -- Create the descriptions for the commands
+    model_desc = "The location of the model XML file"
+    
+    fps_desc = "Number of Frames Per Second for the video"
+    seconds_desc = "The duration of the video in seconds"
+    img_size_desc = "The width or height for the frame to be generated"
+    scale_desc = "Scale factor for inputs"
+    p_c_speed_desc = "The rate of flow/change of the pattern"
+    progress_desc = "Display the progress of frame generation"
+    save_frames_desc = "Save the individual frame generated as PNG images"
 
     # -- Add required and optional groups
     parser._action_groups.pop()
@@ -16,13 +26,13 @@ def get_args():
     # -- Create the arguments
     required.add_argument("--model", help=model_desc, required=True)
     
-    optional.add_argument("--fps", default=15, type=int)
-    optional.add_argument("--seconds", default=10, type=int)
-    optional.add_argument("--img_size", default=200, type=int)
-    optional.add_argument("--scale", default=0.3, type=float)
-    optional.add_argument("--pattern_change_speed", default=0.5, type=float)
-    optional.add_argument("--progress", action="store_true")
-    optional.add_argument("--save_frames", action="store_true")
+    optional.add_argument("--fps", help=fps_desc, default=15, type=int)
+    optional.add_argument("--seconds", help=seconds_desc, default=10, type=int)
+    optional.add_argument("--img_size", help=img_size_desc, default=200, type=int)
+    optional.add_argument("--scale", help=scale_desc, default=0.3, type=float)
+    optional.add_argument("--pattern_change_speed", help=p_c_speed_desc, default=0.5, type=float)
+    optional.add_argument("--progress", help=progress_desc, action="store_true")
+    optional.add_argument("--save_frames", help=save_frames_desc, action="store_true")
     
     args = parser.parse_args()
 
